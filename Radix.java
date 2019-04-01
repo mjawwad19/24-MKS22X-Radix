18f14879f6d626f43a02f9bcd39ac154b9b3e0ad
Mohammad Jawwad <mjawwad@stuy.edu>
2019-04-01 16:01:25 -0400

    added a helper fxn that finds the longest int / max amount of digits

--- Radix.java
+++ Radix.java
@@ -11,11 +11,27 @@ public class Radix{
   public static int getDigit(int n, int place) {
     return (int)(n / Math.pow(10, place)) % 10;
   }
+  //finds the longest number (most places) in an array
+  public static int maxPlaces(int[] data) {
+    int m = 0;
+    for (int n: data) {
+      int temp = n;
+      int count = 0;
+      while (temp != 0) {
+        count++;
+        temp = temp/10;
+      }
+      if (count > m) m = count;
+    }
+    return m;
+  }
 
   public static void main(String[] args) {
     System.out.println(getDigit(123, 0)); //3
     System.out.println(getDigit(1234, 1)); //3
     System.out.println(getDigit(1234, 3)); //1
+    int[] ary = {123, 1234, 12345, -123456};
+    System.out.println(maxPlaces(ary)); //6
   }
 
 
