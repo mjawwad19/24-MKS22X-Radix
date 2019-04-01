f65309a2aacad583766fa1e72ae9a72cd2b42206
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-31 21:18:25 -0400

    updated Driver1 to show only necessary methods working

--- Driver1.java
+++ Driver1.java
@@ -5,7 +5,7 @@ public class Driver1{
 
         //Step One: Creating a valid Instance
         System.out.println("New Instance Test:\n");
-        MyLinkedList list = new MyLinkedList();
+        MyLinkedList<Integer> list = new MyLinkedList<Integer>();
         System.out.println(list);
         System.out.println("This should print: \"[]\"\n");
 
@@ -18,42 +18,11 @@ public class Driver1{
         System.out.println(list);
         System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\"");
 
-        //Step Three: Adding a value to the middle
-        System.out.println("Adding Values in the Middle Test:\n");
-        list.add(5, 999);
-        System.out.println(list);
-        System.out.println("This should print: \"[0, 1, 2, 3, 4, 999, 5, 6, 7, 8, 9]\"");
-
-        //Step Four: Removing a value from the list by index
-        System.out.println("Removing a Value Test:\n");
-        list.remove(5);
-        System.out.println(list);
-        System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\"");
-
-        //Step Five: Remove a value from the list by value
-        System.out.println("Remove a Value by its Value Test:\n");
-        list.remove(new Integer(9));
-        System.out.println(list);
-        System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8]\"");
-        //Step Six: Testing the Contains Method
-        System.out.println("Checking Containment Test:\n");
-        System.out.println(list.contains(new Integer(999)));
-        System.out.println("This should print: false");
 
         //Step Seven: Testing the Size Method
         System.out.println("Size Test: \n");
         System.out.println(list.size());
-        System.out.println("This should print: 9");
-
-        //Step Eight: indexOf Method
-        System.out.println("Index of Value Test: \n");
-        System.out.println(list.indexOf(0));
-        System.out.println("This should print: 0");
-
-        //Step Nine: Testing the reverseString Method
-        //System.out.println(list); //[0, 1, 2, 3, 4, 5, 6, 7, 8]
-        System.out.println(list.reverseString());
-        System.out.println("This should print: \"[8, 7, 6, 5, 4, 3, 2, 1, 0]\" \n");
+        System.out.println("This should print: 10");
 
         //Step Ten: Testing Extend method
         MyLinkedList list2 = new MyLinkedList();
@@ -63,9 +32,9 @@ public class Driver1{
         //System.out.println(list2); //[0, 2, 4, 6, 8]
         list.extend(list2);
         System.out.println(list);
-        System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 2, 4, 6, 8]\"");
+        System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2, 4, 6, 8]\"");
         System.out.println(list.size());
-        System.out.println("This should print: 14");
+        System.out.println("This should print: 15");
         System.out.println(list2);
         System.out.println("This should print: \"[]\"");
         System.out.println(list2.size());
